92d5192062cfc3a72900d145907665b11e5d8f93
Django0 <Django0@users.noreply.github.com>
2018-01-27 16:40:10 +0100

    Short Interrupt handler & added temperature sensor

--- Vetenskap_Arduino_mqtt_esp8266.ino
+++ Vetenskap_Arduino_mqtt_esp8266.ino
@@ -23,9 +23,8 @@
 // Last updated: 2018-01-22
 #define WLAN_SSID       ""
 #define WLAN_PASS       ""
-
 /************************* Adafruit.io Setup *********************************/
-#define AIO_SERVER      "192.168.42.235"
+#define AIO_SERVER      "192.168.43.182"
 #define AIO_SERVERPORT  1883                   // use 8883 for SSL
 #define AIO_USERNAME    ""
 #define AIO_KEY         ""
@@ -33,11 +32,15 @@
 /************ Global State (you don't need to change this!) ******************/
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-const int ledBlue = D1; 
-const int ledRed = D2; 
+const int ledBlue = D1;
+const int ledRed = D2;
 const int readLedGreenInterruptPin = D4;
-uint32_t pressedCount=0;
-uint32_t x=0;
+uint32_t pressedCount = 0;
+uint32_t localPressedCount = pressedCount;
+uint32_t x = 0;
+
+// named constant for the pin the temperature sensor is connected to
+const int temperatureSensorPin = A0;
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -49,10 +52,11 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 
 /****************************** Feeds ***************************************/
 
-// Setup a feed called 'photocell' for publishing.
+// Setup a feed called 'heartbeat' for publishing.
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/photocell");
+Adafruit_MQTT_Publish heartbeat = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/heartbeat");
 Adafruit_MQTT_Publish physicalButton = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/physicalButton");
+Adafruit_MQTT_Publish temperatureReadOut = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temp");
 
 // Setup a feed called 'onoff' for subscribing to changes.
 Adafruit_MQTT_Subscribe onoffbuttonBlue = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoffblue");
@@ -65,14 +69,7 @@ Adafruit_MQTT_Subscribe onoffbuttonRed = Adafruit_MQTT_Subscribe(&mqtt, AIO_USER
 void MQTT_connect();
 //##############################################################################################
 void buttonInterrupted() {
-    Serial.print(F("\nSending physical button state = "));
-    Serial.print(++pressedCount);
-    Serial.print("...");
-    if (! physicalButton.publish(pressedCount)) {
-      Serial.println(F("Failed"));
-    } else {
-      Serial.println(F("OK!"));
-    }
+  pressedCount++;
 }
 //##############################################################################################
 void setup() {
@@ -100,16 +97,16 @@ void setup() {
   mqtt.subscribe(&onoffbuttonBlue);
   mqtt.subscribe(&onoffbuttonRed);
 
-  
+
   pinMode(ledBlue, OUTPUT);
   pinMode(ledRed, OUTPUT);
   pinMode(readLedGreenInterruptPin, INPUT);
-  
+
   digitalWrite(ledBlue, HIGH);
-  delay(1000);  
+  delay(1000);
   digitalWrite(ledBlue, LOW);
-  delay(1000);  
-  
+  delay(1000);
+  attachInterrupt(digitalPinToInterrupt(readLedGreenInterruptPin), buttonInterrupted, HIGH);  // CHANGE is too fast!
 }
 
 //##############################################################################################
@@ -127,7 +124,7 @@ void loop() {
     if (subscription == &onoffbuttonBlue) {
       Serial.print(F("Got: "));
       Serial.println((char *)onoffbuttonBlue.lastread);
-      if (0 == strcmp((const char*)onoffbuttonBlue.lastread,"1")) {
+      if (0 == strcmp((const char*)onoffbuttonBlue.lastread, "1")) {
         digitalWrite(ledBlue, HIGH);
       } else {
         digitalWrite(ledBlue, LOW);
@@ -143,22 +140,62 @@ void loop() {
     }
   }
 
-  // Now we can publish stuff!
-  Serial.print(F("\nSending photocell val "));
+  // Now we can publish heartbeat!
+  Serial.print(F("\nSending heartbeat val "));
   Serial.print(x);
   Serial.print("...");
-  if (! photocell.publish(x++)) {
+  if (! heartbeat.publish(x++)) {
     Serial.println(F("Failed"));
   } else {
     Serial.println(F("OK!"));
   }
 
+  // Now we can publish physical button press!
+  if (pressedCount != localPressedCount) {
+    localPressedCount = pressedCount;
+    Serial.print(F("\nSending physical button state = "));
+    Serial.print(localPressedCount);
+    Serial.print("...");
+    if (! physicalButton.publish(pressedCount)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+    }
+  }
+
+  int sensorVal = analogRead(temperatureSensorPin);
+
+  // send the 10-bit sensor value out the serial port
+  Serial.print("sensor Value: ");
+  Serial.print(sensorVal);
+  // convert the ADC reading to voltage
+  float voltage = (sensorVal / 1024.0) * 3.1;
+  // Send the voltage level out the Serial port
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  // convert the voltage to temperature in degrees C
+  // the sensor changes 10 mV per degree
+  // the datasheet says there's a 500 mV offset
+  // ((volatge - 500mV) times 100)
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+
+  // Now we can publish stuff!
+  Serial.print(F("\nSending temperature: "));
+  Serial.print(temperature);
+  Serial.print("...");
+  if (! temperatureReadOut.publish(temperature)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
   // ping the server to keep the mqtt connection alive
   // NOT required if you are publishing once every KEEPALIVE seconds
   /*
-  if(! mqtt.ping()) {
+    if(! mqtt.ping()) {
     mqtt.disconnect();
-  }
+    }
   */
 }
 //##############################################################################################
@@ -176,16 +213,15 @@ void MQTT_connect() {
 
   uint8_t retries = 3;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
   }
   Serial.println("MQTT Connected!");
-  attachInterrupt(digitalPinToInterrupt(readLedGreenInterruptPin), buttonInterrupted, HIGH);  // CHANGE is too fast!
 }
